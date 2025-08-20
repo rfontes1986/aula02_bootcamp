7e77b08e5ca04b1e2af74dc8dd7d58525ff29a26
Rafael Cavalcanti <rfontes86@gmail.com>
2025-08-20 22:23:10 +0100

    feat: added exercise to main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ("Vai comecar a aula 2")
+#1 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo
+
+numero_01 = int(input("Inserir um número inteiro: "))
+numero_02 = int(input("Inserir outro número inteiro: "))
+
+resultado = numero_01 // numero_02
+
+print (resultado)
