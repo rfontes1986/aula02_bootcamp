cd4cd44e6c50b4840b702e4927ec6a3f99bdccef
Rafael Cavalcanti <rfontes86@gmail.com>
2025-08-22 22:23:02 +0100

    added try except to third exercise

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data_usuario = input ("Digite uma data no formato dd/mm/aaaa: ")
+try:
+    data_usuario = input ("Digite uma data no formato dd/mm/aaaa: ")
 
-lista_dia_mes_ano = data_usuario.split("/")
+    lista_dia_mes_ano = data_usuario.split("/")
 
-print (f"O elemento 1 é o {lista_dia_mes_ano[0]}")
-print (f"O elemento 2 é o {lista_dia_mes_ano[1]}")
-print (f"O elemento 3 é o {lista_dia_mes_ano[2]}")
+    print (f"O elemento 1 é o {lista_dia_mes_ano[0]}")
+    print (f"O elemento 2 é o {lista_dia_mes_ano[1]}")
+    print (f"O elemento 3 é o {lista_dia_mes_ano[2]}")
+except:
+    print ("You didn type a correct date format.")
