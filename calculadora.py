d4f41687f33b30b51ea92ff93b3f929230a50d93
Rafael Cavalcanti <rfontes86@gmail.com>
2025-09-14 22:39:36 +0100

    refactor: calculadora agora completa

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,37 @@
 calculator_input = input("Please fill out a basic calculation in the form of 'x' OP 'y': ")
 
-parts = calculator_input.split(('+'))
+try:
+    if str('+') in calculator_input:
+        parts = calculator_input.split(str('+'))
+        a = int(parts[0])
+        b = int(parts[1])
+        calculator_output = a + b
+    elif str('-') in calculator_input:
+        parts = calculator_input.split(str('-'))
+        a = int(parts[0])
+        b = int(parts[1])
+        calculator_output = a - b
+    elif str('*') in calculator_input:
+        parts = calculator_input.split(str('*'))
+        a = int(parts[0])
+        b = int(parts[1])
+        calculator_output = a * b
+    elif str('/') in calculator_input:
+        parts = calculator_input.split(str('/'))
+        a = int(parts[0])
+        b = int(parts[1])
+        if b == 0:
+            calculator_output = "Not possible, deviding by zero is not allowed!"
+        else:
+            calculator_output = a / b
+    elif str('^') in calculator_input:
+        parts = calculator_input.split(str('^'))
+        a = int(parts[0])
+        b = int(parts[1])
+        calculator_output = a ** b
 
-a = int(parts[0])
-b = int(parts[1])
+    print (calculator_input, "=", calculator_output)
+    
+except NameError:
+    print ("Error!")
 
-calculator_output = a + b
-
-print (calculator_input, "=", calculator_output)
